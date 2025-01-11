8de04cc9e53ea6538806bb09a8d8f305329539b9
AryanSethi20 <aryansethi20@gmail.com>
2025-01-11 14:45:25 +0800

    removed unncecessary files

--- test.py
+++
@@ -1,6 +0,0 @@
-import paho.mqtt.client as mqtt
-
-client = mqtt.Client()
-client.connect("broker.emqx.io", 1883, 60)
-test_message = '{"PAoI": [0.201, 0.202, 0.203], "policy": "CU"}'
-client.publish("artc1/status_update/CU", test_message)
